b1b561a45cd3ec542776c36891b20145fd7c4877
ChinatipJ <chaarllzilia@gmail.com>
2024-12-09 15:55:47 +0700

    Updates

---
+++ js/dom01-simple.js
@@ -0,0 +1,24 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const inputComponents = [
+        ...document.querySelectorAll('input[type="number"].app-elem-input'),
+    ];
+    const computeResult = () => {
+        /* const result = inputComponents[0].value + inputComponents[1].value; */
+        /* const result = inputComponents.reduce((result, inputComponents) => {
+            return result + inputComponents.valueAsNumber;
+          }, 0) */
+        const result = inputComponents.reduce((result, inputComponent) => result + inputComponent.valueAsNumber, 0);
+        const output = document.querySelector('output.app-elem-result');
+        if (output !== null) {
+            output.value = `${result}`;
+        }
+        else {
+            console.error('output.app-elem-result not found');
+        }
+    };
+    inputComponents.forEach((inputComponent) => {
+        inputComponent.addEventListener('change', computeResult);
+    });
+    computeResult();
+});
